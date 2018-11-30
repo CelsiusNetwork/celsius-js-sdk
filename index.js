1ba09b34192b215542af9d07ddb451586e194a6c
JovanovicJovan <jjovanovic24@hotmail.com>
2018-11-30 17:13:44 +0100

    Added routes to wallet-api.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require("./lib/core");
+const { Celsius } = require("./lib/core");
+let config = {};
+
+config.authMethod = 'user-token';
+config.partnerKey = '';
+config.userSecret = 'd41d8cd98f00b204e9800998ecf8427e';
+
+let routes = Celsius(config);
