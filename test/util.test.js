cc048e9f1a4cf4e2b13ec056f5dd123e26f7ceb0
JovanovicJovan <jjovanovic24@hotmail.com>
2019-03-18 11:17:32 +0100

    Added mocha tests.

---
+++ test/util.test.js
@@ -0,0 +1,46 @@
+const {Celsius, AUTH_METHODS, ENVIRONMENT} = require('../index')
+const expect = require('chai').expect
+const {
+  oldUser,
+  partnerKeyToken,
+  baseUrl,
+  publicKey,
+  partnerKeyApiKey
+} = require('./test-config')
+let instance, supportedCurrencies
+
+describe('Util Test', async function () {
+  before(async () => {
+    instance = await Celsius({
+      authMethod: AUTH_METHODS.USER_TOKEN, // Auth method
+      partnerKey: partnerKeyToken, // partner key
+      baseUrl: baseUrl, // Wallet-API url
+      publicKey: publicKey // public key
+    })
+  })
+
+  it('Partner statistics', async () => {
+    let data = await instance.getStatistics(oldUser)
+    expect(data).to.be.a('object')
+  })
+
+  it('Gets supported currencies', async () => {
+    supportedCurrencies = await instance.getSupportedCurrencies()
+    expect(supportedCurrencies).to.be.a('object')
+    expect(supportedCurrencies.currencies).to.be.a('array')
+  })
+
+  it('Interest rates', async () => {
+    let { interestRates } = await instance.getInterestRates()
+    expect(interestRates).to.be.a('array')
+  })
+
+  it('Should creates api-key partner', async () => {
+    let apiKeyPartner = await Celsius({
+      authMethod: AUTH_METHODS.API_KEY, // Auth method
+      partnerKey: partnerKeyApiKey, // partner key
+      environment: ENVIRONMENT.STAGING, // Wallet-API url
+      publicKey: publicKey // public key
+    })
+  })
+})
